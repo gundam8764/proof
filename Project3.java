f82830bdba0672b111baff53210620d59e4b351e
gundam8764 <flaciri0@email.cpcc.edu>
2021-01-30 11:30:06 -0500

    Create Project3.java

---
+++ Project3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Project3
+{
+	
+   public static void main(String[] args)
+		
+      //Project Phase 1 
+		{ 
+                                          
+      }//end main
+                 
+}//end class
